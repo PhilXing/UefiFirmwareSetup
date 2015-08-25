cf01b326ecbbc410646837fa63bd7a62231a6a20
PhilXing <philxing1967@gmail.com>
2015-08-25 15:08:28 +0800

    1. OsIndications shouble be a UINT64 variable. 2. Check OsIndicationSupport before set OsIndications. 3. Re-Flow. 4. RaisePrivileges.

--- UefiFirmwareSetup/UefiFirmwareSetup.cpp
+++ UefiFirmwareSetup/UefiFirmwareSetup.cpp
@@ -1,18 +1,8 @@
 #include <windows.h>
 
 #define EFI_OS_INDICATIONS_BOOT_TO_FW_UI	0x0000000000000001
-#define EFI_GLOBAL_VARIABLE_GUID			L"{8BE4DF61-93CA-11d2-AA0D-00E098032B8C}"
-#define MESSAGE_BOX_TITLE					L"Reboot to UEFI Firmware UI"
-
-
-//#include <stdio.h>
-
-//DWORD WINAPI GetFirmwareEnvironmentVariable(
-//  _In_  LPCTSTR lpName,
-//  _In_  LPCTSTR lpGuid,
-//  _Out_ PVOID   pBuffer,
-//  _In_  DWORD   nSize
-//);
+#define EFI_GLOBAL_VARIABLE_GUID						L"{8BE4DF61-93CA-11d2-AA0D-00E098032B8C}"
+#define MESSAGE_BOX_TITLE									L"Reboot to UEFI Firmware UI"
 
 bool RasiePrivileges(void)
 {
@@ -23,7 +13,6 @@ bool RasiePrivileges(void)
 	if (!OpenProcessToken(GetCurrentProcess(), 
 			TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, 
 			&hToken)) {
-				//printf("Failed OpenProcessToken\r\n");
 				return false;
 	}
 
@@ -46,46 +35,67 @@ bool RasiePrivileges(void)
 	AdjustTokenPrivileges(hToken, FALSE, &tkp, 0, NULL, &len);
 
 	if (GetLastError() != ERROR_SUCCESS) {
-		//printf("Failed RasiePrivileges()\r\n");
 		return false;
 	}
 	return true;
 }
 
-//THE EASIEST WINAPI PROGRAM YOU WILL EVER DO!
+
 int WINAPI WinMain(HINSTANCE hInstance, 
                    HINSTANCE hPrevInstance,
                    LPSTR lpCmdLine, int nCmdShow)
 {
-	DWORD OsIndications;
-
+	UINT64 OsIndicationsSupported;
+	UINT64 OsIndications = 0;
+	//
+	// Raise process privileges for firmware variable access and system reboot
+	//
 	if (!RasiePrivileges()) {
 		MessageBox(NULL, L"Fail to raise Privileges", MESSAGE_BOX_TITLE,  MB_OK | MB_ICONASTERISK);
 		return 1;
 	}
-	GetFirmwareEnvironmentVariable(L"OsIndications",EFI_GLOBAL_VARIABLE_GUID, &OsIndications, sizeof(DWORD));
+	//
+	// Get firmware variable OsIndicationsSupported with error check
+	//
+	GetFirmwareEnvironmentVariable(L"OsIndicationsSupported",EFI_GLOBAL_VARIABLE_GUID, &OsIndicationsSupported, sizeof(OsIndicationsSupported));
     if (GetLastError() ==ERROR_INVALID_FUNCTION) { // This.. is.. LEGACY BIOOOOOOOOS....
-		MessageBox(NULL, L"This is an Legacy Windows", MESSAGE_BOX_TITLE,  MB_OK | MB_ICONASTERISK);
-		return 1;
-    } else{
-		MessageBox(NULL, L"This is an UEFI Windows", MESSAGE_BOX_TITLE, MB_OK | MB_ICONASTERISK);
-		OsIndications = OsIndications | EFI_OS_INDICATIONS_BOOT_TO_FW_UI;
-		if (SetFirmwareEnvironmentVariable(L"OsIndications",EFI_GLOBAL_VARIABLE_GUID, &OsIndications, sizeof(DWORD))) {
-			MessageBox(NULL, L"Set OS Indications success", MESSAGE_BOX_TITLE, MB_OK | MB_ICONASTERISK);
-		   // Shut down the system and force all applications to close.   
-		   if (!ExitWindowsEx(EWX_REBOOT | EWX_FORCE,
-			  SHTDN_REASON_MAJOR_OPERATINGSYSTEM |
-			  SHTDN_REASON_MINOR_UPGRADE |
-			  SHTDN_REASON_FLAG_PLANNED))
-			  MessageBox(NULL, L"Failed to Shut Down the system.", MESSAGE_BOX_TITLE, MB_OK | MB_ICONASTERISK);
+		MessageBox(NULL, L"This is not an UEFI OS", MESSAGE_BOX_TITLE,  MB_OK | MB_ICONASTERISK);
+		return 2;
+    } 
+	//
+	// Check Boot To FW UI capability
+	//
+	if ((OsIndicationsSupported & EFI_OS_INDICATIONS_BOOT_TO_FW_UI) == 0) {
+		MessageBox(NULL, L"Boot To FW UI is not supported by the system firmware.", MESSAGE_BOX_TITLE,  MB_OK | MB_ICONASTERISK);
+		return 3;
+	}
+	//
+	// Get firmware variable OsIndications with error check
+	//
+	GetFirmwareEnvironmentVariable(L"OsIndications",EFI_GLOBAL_VARIABLE_GUID, &OsIndications, sizeof(OsIndications));
+	//
+	// Set indication for Boot to Firmware UI with error check
+	//
+	OsIndications = OsIndications | EFI_OS_INDICATIONS_BOOT_TO_FW_UI;
+	if (!SetFirmwareEnvironmentVariable(L"OsIndications",EFI_GLOBAL_VARIABLE_GUID, &OsIndications, sizeof(DWORD))) {
+	    if (GetLastError() == ERROR_NOACCESS) { // GUID Namespace does not exist....
+			MessageBox(NULL, L"GUID Namespace does not exist", MESSAGE_BOX_TITLE, MB_OK | MB_ICONASTERISK);
 		} else {
-		    if (GetLastError() == ERROR_NOACCESS) { // GUID Namespace does not exist....
-				MessageBox(NULL, L"GUID Namespace does not exist", MESSAGE_BOX_TITLE, MB_OK | MB_ICONASTERISK);
-			} else {
-				MessageBox(NULL, L"Set OS Indications fail", MESSAGE_BOX_TITLE, MB_OK | MB_ICONASTERISK);
-			}
-	        return 2;
+			MessageBox(NULL, L"Set OS Indications fail", MESSAGE_BOX_TITLE, MB_OK | MB_ICONASTERISK);
 		}
-        return 0;
-    }
+        return 5;
+	}
+	//
+	// Reboot the system and force all applications to close
+	//
+	MessageBox(NULL, L"System will reboot to BIOS setup", MESSAGE_BOX_TITLE, MB_OK | MB_ICONASTERISK);
+    if (!ExitWindowsEx(EWX_REBOOT | EWX_FORCE,
+ 	  SHTDN_REASON_MAJOR_OPERATINGSYSTEM |
+ 	  SHTDN_REASON_MINOR_UPGRADE |
+	  SHTDN_REASON_FLAG_PLANNED)) {
+		  MessageBox(NULL, L"Failed to Reboot the system.", MESSAGE_BOX_TITLE, MB_OK | MB_ICONASTERISK);
+          return 6;
+	}
+
+	return 0;
 }
